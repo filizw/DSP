381d7be5b8cfe803958f4561000a9045af74dd8c
filizw <fizworski202@gmail.com>
2024-07-25 16:51:52 +0200

    Add missing examples

--- examples/continuous_signals.jl
+++ examples/continuous_signals.jl
@@ -46,11 +46,20 @@ x = DSP.sawtooth_wave_bl.(t, band = 10)
 
 lines(t, x)
 ## triangular wave bl
+t = range(-1.5, 1.5, 1000)
+x = DSP.triangular_wave_bl.(t, band = 10)
 
+lines(t, x)
 ## square wave bl
+t = range(-1.5, 1.5, 1000)
+x = DSP.square_wave_bl.(t, band = 10)
 
+lines(t, x)
 ## pulse wave bl
+t = range(-1.5, 1.5, 1000)
+x = DSP.pulse_wave_bl.(t, D = 0.5, band = 10)
 
+lines(t, x)
 ## impulse repeater bl
 fb = DSP.impulse_repeater_bl(DSP.ramp_wave, 0, 0.5, 10)
 
